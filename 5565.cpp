58db0fd59dda6033a5ced1c23e4a8a74be58cb26
Your_K <kafu@yourk.me>
2019-08-26 19:39:25 +0900

    5565 index.md
    덜 이쁘길래 index.md 넣어봤어요

---
+++ 5565.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a[9];
+    cin >> a[0] >> a[1] >> a[2] >> a[3] >> a[4] >> a[5] >> a[6] >> a[7] >> a[8] >> a[9];
+    cout << a[0]-a[1]-a[2]-a[3]-a[4]-a[5]-a[6]-a[7]-a[8]-a[9] << endl;
+}
