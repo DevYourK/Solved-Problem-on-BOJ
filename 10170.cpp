08342674f73562fac44c1de879b4ad959af1f517
Your_K <kafu@yourk.me>
2019-08-31 16:22:12 +0900

    9654 10170 15962  15733

---
+++ 10170.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+std::cout << "NFC West       W   L  T" << std::endl << "-----------------------" << std::endl << "Seattle        13  3  0" << std::endl << "San Francisco  12  4  0" << std::endl << "Arizona        10  6  0" << std::endl << "St. Louis      7   9  0";
+std::cout << std::endl << std::endl;
+std::cout << "NFC North      W   L  T" << std::endl << "-----------------------" << std::endl << "Green Bay      8   7  1" << std::endl << "Chicago        8   8  0" << std::endl << "Detroit        7   9  0" << std::endl << "Minnesota      5  10  1";
+}
