6f5db6912bad1fef55c734f92415cb33e4242d68
Your_K <kafu@yourk.me>
2020-05-11 23:57:47 +0900

    #C++
    #언어의 다양화를 위해 기존의 코드를 C++ 폴더로 이동했습니다.

---
+++ C++/09000/9012.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+    stack<char> s;
+    int n;
+    string str;
+
+    cin >> n;
+    for(int i=0; i<n; i++) {
+        cin >> str;
+    }
+}
