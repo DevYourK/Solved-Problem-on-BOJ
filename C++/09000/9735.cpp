8ffe2eb8bdef346b8ab66137ebfbf7c8b5a674bb
yourkme <kafu@your.kme>
2023-01-02 13:24:05 +0900

    9735 -> OutOfBounds
    OutOfBounds Err 발생

--- C++/09000/9735.cpp
+++ C++/09000/9735.cpp
@@ -1,48 +1,77 @@
 #include <iostream>
-#include <cmath>
 #include <algorithm>
 #include <vector>
+#include <cmath>
+
 using namespace std;
 
-pair<float,float>Q(int A, int B, int C) {
-    pair<float,float> X;
+void rf(vector<double> *ans, long long a, long long b, long long c) {
+    //cout << a << " " << b << " " << c << "\n";
+    // 만약 실근이 존재하지 않는다면 그만두기
+    if((b*b) - (4*a*c) < 0) {
+        return;
+    }
 
-    //-B + √ B^2 - 4AC
-    X.first = (-B+sqrt())/2*A*B;
+    // 실근이 존재한다면 근의 공식으로 값 찾기
+    double val_p = (-b+sqrt(b*b-4*a*c))/(2*a);
+    double val_m = (-b-sqrt(b*b-4*a*c))/(2*a);
 
-    //-B + √ B^2 - 4AC
-    X.second = (-B-)/2*A*B;
-
-    return X;
+    (*ans).push_back(val_p);
+    (*ans).push_back(val_m);
 }
 
 int main() {
     ios::sync_with_stdio(0);
-    cin.tie(nullptr);
-    cout.setf(ios::fixed);
-    cout.setf(ios::showpoint);
+    cin.tie(0);
+    cout.tie(0);
+
+    cout << fixed;
     cout.precision(4);
 
     int N;
     cin >> N;
+
+    for(int c=0; c<N; c++) {
+        vector<int> vec(4);
+        vector<double> ans;
+
+        cin >> vec[0] >> vec[1] >> vec[2] >> vec[3]; 
     
-    for(int i=0; i<N; i++) {
-        vector <int>input(4);
-        vector <float>vec;
+        if(vec[3] == 0) {
+            // D = 0인 경우 -> 한 해는 무조건 0(정수)
+            // 정수 하나 존재-> 이차방정식 근의 공식으로 해결
+            ans.push_back(0.0);
 
-        for(int j=0; j<4; j++) {
-            cin >> input[j];
-        }
-        
-        if(input[3]!=0) {
-            // x(Ax^2 + Bx + C) 꼴의 식
-            vec.push_back((float)0);
-
-
+            rf(&ans, vec[0], vec[1], vec[2]);
         } else {
-            int DoA = (input[3]/input[0]);
+            // D != 0인 경우 -> 한 해를 찾기 (정수)
+            // 정수 해를 찾고 나머지는 근의 공식으로 해결
+            int num = 0;
+            for(long long i=-abs(vec[3]); i<=abs(vec[3]); i++) {
+                long long a = vec[0] * pow(i, 3);
+                long long b = vec[1] * pow(i, 2);
+                long long c = vec[2] * i;
 
-            cout << ((DoA>0)?-DoA:DoA ) << "< x <" << ((DoA>0)?DoA:-DoA) << "\n";
+                if(a + b + c + vec[3] == 0) {
+                    ans.push_back((float)i);
+                    break;
+                }
+            }
+
+            int A = vec[0];
+            int B = vec[0] * ans[0] + vec[1];
+            int C = vec[0] * ans[0] * ans[0] + vec[1] * ans[0] + vec[2];
+
+            rf(&ans, A, B, C);
         }
+
+        sort(ans.begin(), ans.end());
+        ans.erase(unique(ans.begin(), ans.end()), ans.end());
+
+        for(int i=0; i<ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+
+        cout << "\n";
     }
 }
